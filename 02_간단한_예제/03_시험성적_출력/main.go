a43d5576bd22c4582e692d64b0b7db6813b9398f
l2jong <gkwkdlawoh@naver.com>
2020-01-04 23:22:02 +0900

    입출력 예제 - 시험성적 출력

---
+++ 02_간단한_예제/03_시험성적_출력/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// fmt 패키지 : 기본적인 입출력을 담당하는 패키지
+// 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램
+func main() {
+	var score int
+	fmt.Scan(&score)
+	if score >= 90 {
+		fmt.Println("A")
+	} else if score >= 80 {
+		fmt.Println("B")
+	} else if score >= 70 {
+		fmt.Println("C")
+	} else if score >= 60 {
+		fmt.Println("D")
+	} else {
+		fmt.Println("F")
+	}
+}
